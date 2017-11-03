7ba18d6fd01391f8de05edfe0e75e1419a699c24
Andrew Ruef <awruef@cs.umd.edu>
2017-11-03 18:41:00 -0400

    Nits to the bounds test.

--- test/CheckedCRewriter/boundary_tests.c
+++ test/CheckedCRewriter/boundary_tests.c
@@ -4,13 +4,18 @@
 // RUN: checked-c-convert %s -- | %clang_cc1 -verify -fcheckedc-extension -x c -
 // expected-no-diagnostics
 
+void do_something(int *a, int b) {
+  *a = b;
+}
+//CHECK: void do_something(_Ptr<int> a, int b) {
+
 void mut(int *a, int b);
-//CHECK: void mut(int* a, int b); 
+//CHECK: void mut(int *a, int b); 
 
 void mut(int *a, int b) {
   *a += b;
 }
-//CHECK: void mut(int* a, int b); 
+//CHECK: void mut(int *a, int b) {
 
 void bad_ctx(void) {
   mut((int*)0x8001000, 1);
